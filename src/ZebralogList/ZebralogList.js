b26800b0d5a4c684cb050cd8795201d1d87e0bb8
scao <sihao.cao@gmail.com>
2019-12-20 02:21:50 -0500

    changed list headers data input method to use state

--- src/ZebralogList/ZebralogList.js
+++ src/ZebralogList/ZebralogList.js
@@ -10,19 +10,34 @@ class ZebralogList extends Component {
 
     static contextType = ZebralogsContext;
 
+    state = {
+        headers: [
+            { id: 'game_date', description: 'Game Date', cn: 'ZebralogList__game_date' },
+            { id: 'site', description: 'Site', cn: 'ZebralogList__site' },
+            { id: 'distance', description: 'Distance (miles)', cn: 'ZebralogList__distance'},
+            { id: 'paid', description: 'Paid?', cn: 'ZebralogList__paid' },
+            { id: 'type', description: 'Type', cn: 'ZebralogList__type' },
+            { id: 'amount', description: 'Amount ($)', cn: 'ZebralogList__amount' },
+            { id: 'notes', description: 'Notes', cn: 'ZebralogList__notes' },
+            { id: 'actions', description: 'Actions', cn: 'ZebralogList__Actions' },
+        ]
+    }
+
+    renderHeaders() {
+        return this.state.headers.map(header => {
+            const { id, description, cn } = header
+                return (
+                    <li key={id} className={cn}>{description}</li>
+                )
+        })
+    }
+
     render() {
         const { zebralogs } = this.context
         return (
             <section className='ZebralogList'>
                 <ul className='Zebralog__ListHeaders'>
-                    <li className='ZebralogList__game_date'>Game Date</li>
-                    <li className='ZebralogList__site'>Site</li>
-                    <li className='ZebralogList__distance'>Distance (miles)</li>
-                    <li className='ZebralogList__paid'>Paid?</li>
-                    <li className='ZebralogList__type'>Type</li>
-                    <li className='ZebralogList__amount'>Amount ($)</li>
-                    <li className='ZebralogList__notes'>Notes</li>
-                    <li className='ZebralogList__Actions'>Actions</li>
+                    {this.renderHeaders()}
                 </ul>
                 <ul className='ZebralogList__List'>
                     {zebralogs.map(zebralog =>
