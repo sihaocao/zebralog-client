3b17287c6ac28036d8b7321d997ac787f9947f9c
scao <sihao.cao@gmail.com>
2019-12-22 10:31:51 -0500

    adding filter option - initial version

--- src/ZebralogList/ZebralogList.js
+++ src/ZebralogList/ZebralogList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import ZebralogsContext from '../ZebralogsContext';
 import ZebralogEntry from '../ZebralogEntry/ZebralogEntry';
 import './ZebralogList.css';
@@ -11,6 +11,7 @@ class ZebralogList extends Component {
     static contextType = ZebralogsContext;
 
     state = {
+        // headers array used for list headers
         headers: [
             { id: 'game_date', description: 'Game Date', cn: 'ZebralogList__game_date' },
             { id: 'site', description: 'Site', cn: 'ZebralogList__site' },
@@ -20,7 +21,9 @@ class ZebralogList extends Component {
             { id: 'amount', description: 'Amount ($)', cn: 'ZebralogList__amount' },
             { id: 'notes', description: 'Notes', cn: 'ZebralogList__notes' },
             { id: 'actions', description: 'Actions', cn: 'ZebralogList__Actions' },
-        ]
+        ],
+        // site used for 'search by site' filter functionality
+        site: '',
     }
 
     renderHeaders() {
@@ -32,22 +35,44 @@ class ZebralogList extends Component {
         })
     }
 
+    onChange = e => {
+        this.setState({
+            site: e.target.value
+        });
+    }
+
     render() {
         const { zebralogs } = this.context
         return (
-            <section className='ZebralogList'>
-                <ul className='Zebralog__ListHeaders'>
-                    {this.renderHeaders()}
-                </ul>
-                <ul className='ZebralogList__List'>
-                    {zebralogs.map(zebralog =>
-                        <ZebralogEntry
-                            key={zebralog.id}
-                            {...zebralog}
-                        />
-                    )}
-                </ul>
-            </section>
+            <Fragment>
+                <form className='ZebralogList__FormGroup'>
+                    <label htmlFor='search_for'>
+                        Search by:
+                        {' '}
+                    </label>
+                    <input
+                        type='text'
+                        className='Zebralogs__Site__SearchBox'
+                        placeholder='site description'
+                        name='site'
+                        value={this.state.site}
+                        onChange={this.onChange}
+                    />
+                </form>
+                <section className='ZebralogList'>
+                    <ul className='Zebralog__ListHeaders'>
+                        {this.renderHeaders()}
+                    </ul>
+                    <ul className='ZebralogList__List'>
+                        {zebralogs.map(zebralog =>
+                            <ZebralogEntry
+                                key={zebralog.id}
+                                {...zebralog}
+                            />
+                        )}
+                    </ul>
+                </section>
+            </Fragment>
         )
     }
 }
