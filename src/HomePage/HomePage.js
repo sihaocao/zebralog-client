d555d64627bd03e9ad508a4b06a7b1cdb4fc46bd
scao <sihao.cao@gmail.com>
2019-12-29 13:45:50 -0500

    more updates

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import ZebralogsContext from '../ZebralogsContext';
-import LandingPage from './LandingPage/LandingPage';
-import ZebralogList from './ZebralogList/ZebralogList';
+import LandingPage from '../LandingPage/LandingPage';
+import ZebralogList from '../ZebralogList/ZebralogList';
 import './HomePage.css';
 
 class HomePage extends Component {
     render() {
+        console.log(ZebralogsContext.landingPageVisited)
         return (
-            if (!ZebralogsContext.landingPageVisited) {
-                <LandingPage />
-            } else {
-                <ZebralogList />
-            }
+            // if (!ZebralogsContext.landingPageVisited) {
+            //     <LandingPage />
+            // } else {
+            //     <ZebralogList />
+            // }
         )
     }
 }
