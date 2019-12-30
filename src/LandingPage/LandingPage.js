bf8f3d7c89e10a935771973d29e1a210c192b6e9
scao <sihao.cao@gmail.com>
2019-12-29 21:15:12 -0500

    landing page formatting

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -5,7 +5,7 @@ class LandingPage extends Component {
     render() {
         return (
                 <div className='landing-page'>
-                    <h1>Welcome to Zebralogs!</h1>
+                    <h1>Hello there!</h1>
                     <h3 className='app-description'>This app allows any sports official to track their game details and payment information. It includes a 'Download CSV' button for organized record keeping.</h3>
                     <button onClick={this.props.exitLandingPage}>
                         Let's Get Started!
